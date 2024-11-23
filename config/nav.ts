ec5ba929378db55cabab5052d8ada51179a05566
xukaiyyds <3353212714@qq.com>
2024-11-23 14:43:53 +0800

    update

--- config/nav.ts
+++ config/nav.ts
@@ -2,7 +2,6 @@ export const navigationItems = [
   { href: '/', text: '首页' },
   { href: '/blog', text: '博客' },
   { href: '/projects', text: '项目' },
-  { href: '/about', text: '作品' },
   { href: '/guestbook', text: '留言墙' },
   // { href: '/ama', text: 'AMA 咨询' },
   // { href: '/about', text: '关于' },
